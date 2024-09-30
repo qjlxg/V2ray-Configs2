fe542ea4e2bf64b3b02dc2f8e68b1ae9cefed1e2
qjlxg <qjlxg@163.com>
2024-09-30 11:15:13 +0800

    Update app.py

--- Files/app.py
+++ Files/app.py
@@ -87,7 +87,12 @@ def main():
         "https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/ss",
         "https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/ssr",
         "https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/tr",
-        "https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/v2"
+        "https://github.com/qjlxg/Sub2/raw/refs/heads/main/sub/share/v2",
+        "https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/available",
+        "https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/ss",
+        "https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/ssr",
+        "https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/tr",
+        "https://github.com/qjlxg/YBSubCrawler/raw/refs/heads/main/sub/share/v2"
         
     ]
     dir_links = [
